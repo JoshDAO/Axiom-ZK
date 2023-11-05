dc36ef00503b6df34ce211ea5722a185a0fd95e3
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-11-05 20:13:09 +0000

    fix merge error

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -178,7 +178,6 @@ contract Marketplace {
             block.timestamp > _expiry + 604800,
             "a week since expiry must have elapsed"
         );
-<<<<<<< HEAD
         // check that over 1 week has elapsed since expiration
         // this is in lieu of proving that the option did NOT expire itm since this is v hard with zk proofs
         // this gives buyers 1 week to redeem their payout after expiry, or they forfeit it.
@@ -187,8 +186,6 @@ contract Marketplace {
             block.timestamp > _expiry + 604800,
             "a week since expiry must have elapsed"
         );
-=======
->>>>>>> 983bd7e (fix duplicated code)
 
         uint256 numberOfContracts = optionSaleInfo[otokenAddress]
             .numberContractsMatched;
