72bb69825acbd7f9c50dc0229c1c2c6fa859e3a0
thundering-silence <25010047+thundering-silence@users.noreply.github.com>
2023-11-05 19:45:41 +0100

    stuff

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,7 @@ require("@nomicfoundation/hardhat-toolbox")
 module.exports = {
 	solidity: "0.8.20",
 	networks: {
-		localhost: {
-			url: "http://127.0.0.1:8545",
+		hardhat: {
 			chainId: 31337,
 			forking: {
 				url: "https://eth-goerli.g.alchemy.com/v2/DDQFORCbIn88uKaaVK0LJVOyFE51hhO3"
